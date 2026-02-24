079d4dba9b7fe5ea1c869b3404e1d6373ecd8899
YuriyGl86 <yuriygl86@gmail.com>
2026-02-24 16:28:22 +0300

    stat-server

--- stats-service/stats-server/src/main/java/ru/practicum/stats/server/StatsServer.java
+++ stats-service/stats-server/src/main/java/ru/practicum/stats/server/StatsServer.java
@@ -1,7 +1,10 @@
 package ru.practicum.stats.server;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StatsServer {
---
+++ stats-service/stats-server/src/main/java/ru/practicum/stats/server/service/StatsServiceImpl.java
@@ -0,0 +1,43 @@
+package ru.practicum.stats.server.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.stats.server.repository.StatsRepository;
+import ru.practicum.stats.server.dto.HitDto;
+import ru.practicum.stats.server.dto.StatsMapper;
+import ru.practicum.stats.server.dto.ViewStatsDto;
+import ru.practicum.stats.server.model.EndpointHit;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class StatsServiceImpl implements StatsService {
+    private final StatsRepository repository;
+
+    private static final DateTimeFormatter FORMATTER =
+            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    @Transactional
+    public void saveHit(HitDto hitDto) {
+        EndpointHit hit = StatsMapper.toEntity(hitDto);
+        repository.save(hit);
+        log.debug("Saved hit: {} {} from {}", hit.getApp(), hit.getUri(), hit.getIp());
+    }
+
+    @Override
+    public List<ViewStatsDto> getStats(
+            LocalDateTime start,
+            LocalDateTime end,
+            List<String> uris,
+            boolean unique
+    ) {
+        return repository.getStats(start, end, uris, unique);
+    }
+}
